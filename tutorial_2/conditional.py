911250dd0b5f7fb5e2ff2f401525f5512865782a
Arun Kumar <java.kumar.arun@gmail.com>
2025-12-17 15:01:11 +0530

    practice problems

---
+++ tutorial_2/conditional.py
@@ -0,0 +1,24 @@
+# decision making statements are conditional statements
+# if else
+
+num = 43
+
+# find if given number is even or odd
+if num%2==0:
+    print(num,"is even")
+# if num%2!=0:
+# if num%2==1:
+else:
+    print(num,"is odd")
+
+
+# find larger number
+a = 23
+b = 23
+
+if (a>b):
+    print(f"Larger number is {a}")
+elif (a<b):
+    print(f"Larger number is {b}")
+else:
+    print(f"Both are {a}")
