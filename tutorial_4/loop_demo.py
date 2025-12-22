feec40de319c1f1799c60e57db16023504cdb757
Arun Kumar <java.kumar.arun@gmail.com>
2025-12-22 14:37:35 +0530

    loop

---
+++ tutorial_4/loop_demo.py
@@ -0,0 +1,13 @@
+num = 8
+
+# for(int i=2;i<num;i++){
+    
+# }
+
+# 1 2 3 4
+# for i in range(2,num):
+#     print(i)
+    
+    
+for i in range(1, 11):
+    print(f"{num} * {i} = {num * i}")
--- tutorial_4/string_demo3.py
+++ tutorial_4/string_demo3.py
@@ -1,4 +1,37 @@
-description = "thIS iS a ManGO. I liKE tO EAt mANGo."
+# description = "thIS iS a ManGO. I liKE tO EAt mANGo."
 
 # This Is A Mango. I Like To Eat Mango.
 
+
+
+# description = "thIS iS ManGO."
+# description = "thIS iS a ManGO."
+description = "thIS iS a ManGO. I liKE tO EAt mANGo."
+
+# print(description.capitalize())
+
+# break all words, break wherever space is found
+words = description.split()
+print("Total words count: ",len(words))
+# ['thIS', 'iS', 'a', 'ManGO.']
+
+# print(words[0]) - 'thIS' -> [2] - I
+# print(words[1]) - 'iS' -> [1] - S
+# print(words[2]) - 'a'
+# print(words[3]) - 'ManGO.'
+
+
+# print(words[0].capitalize()) 
+# print(words[1].capitalize()) 
+# print(words[2].capitalize()) 
+# print(words[3].capitalize()) 
+
+words[0] = words[0].capitalize() 
+words[1] = words[1].capitalize() 
+words[2] = words[2].capitalize() 
+words[3] = words[3].capitalize() 
+
+print(words)
+
+
+print(" ".join(words))
